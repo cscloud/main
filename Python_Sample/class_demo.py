e61ffda69fb2a1925839d37d37d1c8fb14ff820e
Yariv Levi <Yariv.Levi@outlook.com>
2015-07-13 10:08:59 +0300

    init

--- Python_Sample/class_demo.py
+++ Python_Sample/class_demo.py
@@ -1,3 +1,6 @@
 class Animal:
            """A generic snimal"""
            kind = 'any' # class attribute, shared between instances
+
+           def __init__(self, name): # Initializer, called in object creation
+                      self.name = name # instance attribute
